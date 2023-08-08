42fde7044676c503c964c485efc1edfb2e0ccb62
Nickiel12 <nicholasyoungsumner@gmail.com>
2023-08-07 19:08:03 -0700

    docs(app.rs): fixed spelling error

--- ratatui-book-tutorial-project/src/app.rs
+++ ratatui-book-tutorial-project/src/app.rs
@@ -24,7 +24,7 @@ pub enum CurrentlyEditing {
 pub struct App {
     pub key_input: String, // the currently being edited json key.
     pub value_input: String, // the currently being edited json value.
-    pub pairs: HashMap<String, String>, // The representation of our key and value pairs with serde Serailize support
+    pub pairs: HashMap<String, String>, // The representation of our key and value pairs with serde Serialize support
     pub current_screen: CurrentScreen, // the current screen the user is looking at, and will later determine what is rendered.
     pub currently_editing: Option<CurrentlyEditing>, // the optional state containing which of the key or value pair the user is editing. It is an option, because when the user is not directly editing a key-value pair, this will be set to `None`.
 }
