a25e8254c686e8d96c55ce9f432d857dcb0d3281
kdheepak <kdheepak@users.noreply.github.com>
2023-08-29 06:45:01 +0000

    Deploy preview for PR 45 🛫

---
+++ pr-preview/pr-45/tutorial/counter-app/ratatui-counter-app/src/ui.rs
@@ -0,0 +1,30 @@
+use ratatui::{
+  layout::Alignment,
+  style::{Color, Style},
+  widgets::{Block, BorderType, Borders, Paragraph},
+};
+
+use crate::{app::App, tui::Frame};
+
+pub fn render(app: &mut App, f: &mut Frame) {
+  f.render_widget(
+    Paragraph::new(format!(
+      "
+        Press `Esc`, `Ctrl-C` or `q` to stop running.\n\
+        Press `j` and `k` to increment and decrement the counter respectively.\n\
+        Counter: {}
+      ",
+      app.counter
+    ))
+    .block(
+      Block::default()
+        .title("Counter App")
+        .title_alignment(Alignment::Center)
+        .borders(Borders::ALL)
+        .border_type(BorderType::Rounded),
+    )
+    .style(Style::default().fg(Color::Yellow))
+    .alignment(Alignment::Center),
+    f.size(),
+  )
+}
