db1df0c999a29cd2e38ed8c5e6851d4a5c883403
kdheepak <kdheepak@users.noreply.github.com>
2023-08-29 01:35:22 +0000

    Deploy preview for PR 41 🛫

---
+++ pr-preview/pr-41/tutorial/counter-app/ratatui-counter-app/src/app.rs
@@ -0,0 +1,40 @@
+///// ANCHOR: application
+/// Application.
+#[derive(Debug, Default)]
+pub struct App {
+  /// should the application exit?
+  pub should_quit: bool,
+  /// counter
+  pub counter: u8,
+}
+///// ANCHOR_END: application
+
+///// ANCHOR: application_impl
+impl App {
+  /// Constructs a new instance of [`App`].
+  pub fn new() -> Self {
+    Self::default()
+  }
+
+  /// Handles the tick event of the terminal.
+  pub fn tick(&self) {
+  }
+
+  /// Set running to false to quit the application.
+  pub fn quit(&mut self) {
+    self.should_quit = true;
+  }
+
+  pub fn increment_counter(&mut self) {
+    if let Some(res) = self.counter.checked_add(1) {
+      self.counter = res;
+    }
+  }
+
+  pub fn decrement_counter(&mut self) {
+    if let Some(res) = self.counter.checked_sub(1) {
+      self.counter = res;
+    }
+  }
+}
+///// ANCHOR_END: application_impl
