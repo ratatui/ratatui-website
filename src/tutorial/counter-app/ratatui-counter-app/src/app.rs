d35978f7e2939978fa28ea936413eecada913cda
Raffaele Cataldo <kemanicataldo@gmail.com>
2023-09-08 20:23:21 -0700

    include more expansive view of working code in examples

--- src/tutorial/counter-app/ratatui-counter-app/src/app.rs
+++ src/tutorial/counter-app/ratatui-counter-app/src/app.rs
@@ -1,10 +1,10 @@
 ///// ANCHOR: action
 pub enum Action {
-  Tick,
-  Increment,
-  Decrement,
-  Quit,
-  None,
+    Tick,
+    Increment,
+    Decrement,
+    Quit,
+    None,
 }
 ///// ANCHOR_END: action
 
@@ -12,39 +12,56 @@ pub enum Action {
 /// Application.
 #[derive(Debug, Default)]
 pub struct App {
-  /// should the application exit?
-  pub should_quit: bool,
-  /// counter
-  pub counter: u8,
+    /// should the application exit?
+    pub should_quit: bool,
+    /// counter
+    pub counter: u8,
 }
 ///// ANCHOR_END: application
 
 ///// ANCHOR: application_impl
 impl App {
-  /// Constructs a new instance of [`App`].
-  pub fn new() -> Self {
-    Self::default()
-  }
-
-  /// Handles the tick event of the terminal.
-  pub fn tick(&self) {
-  }
-
-  /// Set running to false to quit the application.
-  pub fn quit(&mut self) {
-    self.should_quit = true;
-  }
-
-  pub fn increment_counter(&mut self) {
-    if let Some(res) = self.counter.checked_add(1) {
-      self.counter = res;
+    /// Constructs a new instance of [`App`].
+    pub fn new() -> Self {
+        Self::default()
     }
-  }
 
-  pub fn decrement_counter(&mut self) {
-    if let Some(res) = self.counter.checked_sub(1) {
-      self.counter = res;
+    /// Handles the tick event of the terminal.
+    pub fn tick(&self) {}
+
+    /// Set running to false to quit the application.
+    pub fn quit(&mut self) {
+        self.should_quit = true;
     }
-  }
+
+    pub fn increment_counter(&mut self) {
+        if let Some(res) = self.counter.checked_add(1) {
+            self.counter = res;
+        }
+    }
+
+    pub fn decrement_counter(&mut self) {
+        if let Some(res) = self.counter.checked_sub(1) {
+            self.counter = res;
+        }
+    }
 }
 ///// ANCHOR_END: application_impl
+
+///// ANCHOR: application_test
+mod tests {
+    #[test]
+    fn test_app_increment_counter() {
+        let mut app = super::App::default();
+        app.increment_counter();
+        assert_eq!(app.counter, 1);
+    }
+
+    #[test]
+    fn test_app_decrement_counter() {
+        let mut app = super::App::default();
+        app.decrement_counter();
+        assert_eq!(app.counter, 0);
+    }
+}
+///// ANCHOR_END: application_test
---
+++ src/tutorial/counter-app/ratatui-counter-app/src/lib.rs
@@ -0,0 +1,18 @@
+///// ANCHOR: lib_mods
+#![allow(dead_code)]
+
+/// Application.
+pub mod app;
+
+/// Terminal events handler.
+pub mod event;
+
+/// Widget renderer.
+pub mod ui;
+
+/// Terminal user interface.
+pub mod tui;
+
+/// Application updater.
+pub mod update;
+///// ANCHOR_END: lib_mods
