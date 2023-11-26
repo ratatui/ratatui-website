96ac8e096a6fa4b8e4d3d80568dc16280571fa43
Dheepak Krishnamurthy <me@kdheepak.com>
2023-11-26 01:01:06 -0500

    feat: Remove mermaid remark plugin ✨

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
-import starlightLinksValidator from 'starlight-links-validator'
-import remarkMermaid from '@southball/remark-mermaid';
-import remarkIncludeCode from './src/plugin/remark-code-import';
-import emoji from 'remark-emoji';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import starlightLinksValidator from "starlight-links-validator";
+import remarkMermaid from "@southball/remark-mermaid";
+import remarkIncludeCode from "./src/plugin/remark-code-import";
+import emoji from "remark-emoji";
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,123 +12,124 @@ export default defineConfig({
   markdown: {
     remarkPlugins: [
       remarkIncludeCode,
-      remarkMermaid,
+      // remarkMermaid,
       emoji,
     ],
   },
   integrations: [
     starlightLinksValidator(),
     starlight({
-      title: 'Ratatui',
+      title: "Ratatui",
       social: {
-        github: 'https://github.com/ratatui-org/ratatui',
+        github: "https://github.com/ratatui-org/ratatui",
       },
       sidebar: [
         {
-          label: 'Introduction',
-          link: '/introduction/'
+          label: "Introduction",
+          link: "/introduction/",
         },
         {
-          label: 'Installation',
-          link: '/installation/'
+          label: "Installation",
+          link: "/installation/",
         },
         {
-          label: 'Tutorial',
+          label: "Tutorial",
           items: [
             {
-              label: 'Hello World',
-              link: '/tutorial/hello-world/'
+              label: "Hello World",
+              link: "/tutorial/hello-world/",
             },
             {
-              label: 'Counter App',
+              label: "Counter App",
               items: [
                 {
-                  label: 'Single Function',
-                  link: '/tutorial/counter-app/single-function'
+                  label: "Single Function",
+                  link: "/tutorial/counter-app/single-function",
                 },
                 {
-                  label: 'Multiple Functions',
-                  link: '/tutorial/counter-app/multiple-functions'
+                  label: "Multiple Functions",
+                  link: "/tutorial/counter-app/multiple-functions",
                 },
                 {
-                  label: 'Multiple Files',
+                  label: "Multiple Files",
                   items: [
-                    { label: 'Introduction', link: '/tutorial/counter-app/multiple-files' },
-                    { label: 'app.rs', link: '/tutorial/counter-app/multiple-files/app' },
-                    { label: 'ui.rs', link: '/tutorial/counter-app/multiple-files/ui' },
-                    { label: 'event.rs', link: '/tutorial/counter-app/multiple-files/event' },
-                    { label: 'tui.rs', link: '/tutorial/counter-app/multiple-files/tui' },
-                    { label: 'update.rs', link: '/tutorial/counter-app/multiple-files/update' },
-                    { label: 'main.rs', link: '/tutorial/counter-app/multiple-files/main' }
-                  ]
-                }
-              ]
-            }],
+                    { label: "Introduction", link: "/tutorial/counter-app/multiple-files" },
+                    { label: "app.rs", link: "/tutorial/counter-app/multiple-files/app" },
+                    { label: "ui.rs", link: "/tutorial/counter-app/multiple-files/ui" },
+                    { label: "event.rs", link: "/tutorial/counter-app/multiple-files/event" },
+                    { label: "tui.rs", link: "/tutorial/counter-app/multiple-files/tui" },
+                    { label: "update.rs", link: "/tutorial/counter-app/multiple-files/update" },
+                    { label: "main.rs", link: "/tutorial/counter-app/multiple-files/main" },
+                  ],
+                },
+              ],
+            },
+          ],
         },
         {
-          label: 'Concepts',
+          label: "Concepts",
           items: [
             {
-              label: 'Rendering',
-              link: './concepts/rendering',
+              label: "Rendering",
+              link: "./concepts/rendering",
               items: [
                 {
-                  label: 'Under the hood',
-                  link: './concepts/rendering-under-the-hood'
-                }
-              ]
+                  label: "Under the hood",
+                  link: "./concepts/rendering-under-the-hood",
+                },
+              ],
             },
             {
-              label: 'Layout',
-              link: './concepts/layout'
+              label: "Layout",
+              link: "./concepts/layout",
             },
             {
-              label: 'Application Patterns',
+              label: "Application Patterns",
               items: [
                 {
-                  label: 'The Elm Architecture',
-                  link: './concepts/application-patterns/the-elm-architecture'
+                  label: "The Elm Architecture",
+                  link: "./concepts/application-patterns/the-elm-architecture",
                 },
                 {
-                  label: 'Component Architecture',
-                  link: './concepts/application-patterns/component-architecture'
+                  label: "Component Architecture",
+                  link: "./concepts/application-patterns/component-architecture",
                 },
                 {
-                  label: 'Flux Architecture',
-                  link: './concepts/application-patterns/flux-architecture'
-                }
-              ]
+                  label: "Flux Architecture",
+                  link: "./concepts/application-patterns/flux-architecture",
+                },
+              ],
             },
             {
-              label: 'Backends',
+              label: "Backends",
               items: [
                 {
-                  label: 'Comparison',
-                  link: './concepts/backends/comparison'
+                  label: "Comparison",
+                  link: "./concepts/backends/comparison",
                 },
                 {
-                  label: 'Raw Mode',
-                  link: './concepts/backends/raw-mode'
+                  label: "Raw Mode",
+                  link: "./concepts/backends/raw-mode",
                 },
                 {
-                  label: 'Alternate Screen',
-                  link: './concepts/backends/alternate-screen'
+                  label: "Alternate Screen",
+                  link: "./concepts/backends/alternate-screen",
                 },
                 {
-                  label: 'Mouse Capture',
-                  link: './concepts/backends/mouse-capture'
-                }
-              ]
+                  label: "Mouse Capture",
+                  link: "./concepts/backends/mouse-capture",
+                },
+              ],
             },
             {
-              label: 'Event Handling',
-              link: './concepts/event-handling'
-            }
-          ]
-        }
+              label: "Event Handling",
+              link: "./concepts/event-handling",
+            },
+          ],
+        },
       ],
       editLink: {
-        baseUrl: 'https://github.com/ratatui-org/ratatui-website/edit/main/',
+        baseUrl: "https://github.com/ratatui-org/ratatui-website/edit/main/",
       },
     }),
   ],
