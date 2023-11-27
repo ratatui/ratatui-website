24804cdff947010e1b4077c52d20ba5bfb0839b6
Dheepak Krishnamurthy <me@kdheepak.com>
2023-11-26 21:59:05 -0500

    feat: Add quickstart to landing page ✨

---
+++ code/quickstart-ratatui/src/main.rs
@@ -0,0 +1,25 @@
+use ratatui::{prelude::*, widgets::*};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+  let mut terminal = Terminal::new(
+    CrosstermBackend::new(std::io::stdout())
+  )?;
+
+  terminal.draw(|f| {
+
+    let widget = Paragraph::new("Hello world!")
+      .alignment(Alignment::Center)
+      .block(Block::new().borders(Borders::ALL));
+
+    let area = f.size()
+        .inner(&Margin { horizontal: 2, vertical: 2 });
+
+    f.render_widget(widget, area);
+
+  })?;
+
+  std::thread::sleep(std::time::Duration::from_secs(5));
+
+  Ok(())
+}
