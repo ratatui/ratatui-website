2d9dbc71f10cf110703cf62e4ce0e41cedc7fd84
Josh McKinney <joshka@users.noreply.github.com>
2023-12-09 14:37:37 -0800

    Use some nicer colors
    Blue > brown
    https://starlight.astro.build/guides/css-and-tailwind/\#theming

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,17 +1,17 @@
 import starlightPlugin from "@astrojs/starlight-tailwind";
 import colors from "tailwindcss/colors";
 
+// Generated color palettes
+const accent = { 200: '#b5cbe7', 600: '#2a6cba', 900: '#173355', 950: '#14253a' };
+const gray = { 100: '#f1f7ff', 200: '#e4eeff', 300: '#b5c3d9', 400: '#738cb6', 500: '#41587f', 700: '#22385c', 800: '#112648', 900: '#0e1828' };
+
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
-      colors: {
-        // Your preferred accent color. Indigo is closest to Starlight’s defaults.
-        accent: colors.indigo,
-        // Your preferred gray scale. Zinc is closest to Starlight’s defaults.
-        gray: colors.zinc,
-      },
+      colors: { accent, gray },
     },
   },
   plugins: [require("tailwindcss-animate"), starlightPlugin()],
