e3bd118fcfe3e2e836a2ea026bcc7bfcbb18d91d
Valentin271 <valentin.d77@free.fr>
2023-12-14 19:53:55 +0100

    fix: add back missing favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -39,7 +39,7 @@ export default defineConfig({
         light: "./src/assets/logo-light.png",
         replacesTitle: true,
       },
-      favicon: "./src/assets/logo.png",
+      favicon: "/favicon.png",
       components: {
         Header: "./src/components/Header.astro",
       },
