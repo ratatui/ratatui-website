b5fb5ca1bf242a1d7b3fc5ccb2124c7b77b47d2e
Josh McKinney <joshka@users.noreply.github.com>
2024-01-02 12:32:34 -0800

    Tweak table showcase example

--- code/widget-showcase/src/examples/table.rs
+++ code/widget-showcase/src/examples/table.rs
@@ -1,58 +1,67 @@
 use ratatui::{prelude::*, widgets::*};
 
-pub fn render(frame: &mut Frame) {
-    let rows = [
-        Row::new([
-            "1".bold(),
-            "joshka".into(),
-            "john.doe@mail.org".dim(),
-            "UTC -8".into(),
-        ]),
-        Row::new([
-            "2".bold(),
-            "kdheepak".into(),
-            "joe.dane@mail.org".dim(),
-            "UTC -5".into(),
-        ]),
-        Row::new([
-            "3".bold(),
-            "mindoodoo".into(),
-            "jane.doe@mail.org".dim(),
-            "UTC +7".into(),
-        ]),
-        Row::new([
-            "4".bold(),
-            "orhun".into(),
-            "john.smith@mail.org".dim(),
-            "UTC +3".into(),
-        ]),
-        Row::new([
-            "5".bold(),
-            "sayanarijit".into(),
-            "john.smith@mail.org".dim(),
-            "UTC +5:30".into(),
-        ]),
-    ];
+struct Item {
+    user: &'static str,
+    mail: &'static str,
+    timezone: &'static str,
+}
 
+impl Item {}
+
+const ITEMS: [Item; 6] = [
+    Item {
+        user: "fdehau",
+        mail: "Florian Dehau <fdehau@users.noreply.github.com",
+        timezone: "UTC+1",
+    },
+    Item {
+        user: "joshka",
+        mail: "Josh McKinney <joshka@users.noreply.github.com>",
+        timezone: "UTC-8",
+    },
+    Item {
+        user: "kdheepak",
+        mail: "Dheepak Krishnamurthy <me@kdheepak.com>",
+        timezone: "UTC-5",
+    },
+    Item {
+        user: "mindoodoo",
+        mail: "Leon Sautour <minindoo@users.noreply.github.com>",
+        timezone: "UTC+1",
+    },
+    Item {
+        user: "orhun",
+        mail: "Orhun Parmaksız <orhun@archlinux.org>",
+        timezone: "UTC+3",
+    },
+    Item {
+        user: "Valentin271",
+        mail: "Valentin271 <36198422+Valentin271@users.noreply.github.com>",
+        timezone: "UTC+1",
+    },
+];
+
+pub fn render(frame: &mut Frame) {
+    let rows = ITEMS.iter().map(|item| {
+        Row::new([
+            item.user.into(),
+            item.mail.dim().into(),
+            Line::from(item.timezone).alignment(Alignment::Right),
+        ])
+    });
     // width of 46
     let table = Table::new(
         rows,
         [
-            Constraint::Length(2),
             Constraint::Length(11),
-            Constraint::Length(19),
-            Constraint::Length(9),
+            Constraint::Length(29),
+            Constraint::Length(5),
         ],
     )
-    .header(
-        Row::new(["id", "name", "mail", "timezone"])
-            .bold()
-            .underlined()
-            .blue(),
-    )
+    .header(Row::new(["User", "Email", "TZ"]).bold().underlined().blue())
     .highlight_spacing(HighlightSpacing::Always)
     .highlight_style(Style::new().reversed().bold())
-    .highlight_symbol("> ");
+    .highlight_symbol(">");
 
     let mut state = TableState::new().with_selected(Some(4));
 
