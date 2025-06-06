7c78b1a2b9a059c25b46c39726f7c848b7d16609
Josh McKinney <joshka@users.noreply.github.com>
2025-06-06 15:49:04 -0700

    Make a landing page

--- code/tutorials/quickstart-ratatui/src/main.rs
+++ code/tutorials/quickstart-ratatui/src/main.rs
@@ -1,27 +1,17 @@
-use ratatui::{
-    backend::CrosstermBackend,
-    layout::Margin,
-    widgets::{Block, Paragraph},
-    Terminal,
-};
+use ratatui::style::Stylize;
+use ratatui::widgets::{Block, Paragraph};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut terminal = Terminal::new(CrosstermBackend::new(std::io::stdout()))?;
-
+    let mut terminal = ratatui::init();
     terminal.draw(|frame| {
-        let widget = Paragraph::new("Hello world!")
+        let block = Block::bordered().title("Welcome");
+        let greeting = Paragraph::new("Hello, Ratatui! 🐭")
             .centered()
-            .block(Block::bordered());
-
-        let area = frame.area().inner(Margin {
-            horizontal: 2,
-            vertical: 2,
-        });
-
-        frame.render_widget(widget, area);
+            .on_cyan()
+            .block(block);
+        frame.render_widget(greeting, frame.area());
     })?;
-
     std::thread::sleep(std::time::Duration::from_secs(5));
-
+    ratatui::restore();
     Ok(())
 }
