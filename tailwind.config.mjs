87bf7e802f516f38fa94232b104d3c59182d0d10
Shingo OKAWA <shingo.okawa.g.h.c@gmail.com>
2025-10-04 02:41:30 +0900

    chore: bump astro tailwindcss
    Signed-off-by: Shingo OKAWA <shingo.okawa.g.h.c@gmail.com>

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,10 +4,5 @@ import colors from "tailwindcss/colors";
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    extend: {
-      colors: { accent: colors.blue, gray: colors.slate },
-    },
-  },
   plugins: [require("tailwindcss-animate"), starlightPlugin()],
 };
