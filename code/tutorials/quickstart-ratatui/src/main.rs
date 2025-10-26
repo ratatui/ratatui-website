e36c79c6ec4e8b269888862476991fe97b442ea1
Cyberphinx <88593809+Cyberphinx@users.noreply.github.com>
2025-10-26 17:13:34 +0000

    refactor: updated code example snippet, added version number and copy button

--- code/tutorials/quickstart-ratatui/src/main.rs
+++ code/tutorials/quickstart-ratatui/src/main.rs
@@ -2,16 +2,16 @@ use ratatui::style::Stylize;
 use ratatui::widgets::{Block, Paragraph};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut terminal = ratatui::init();
-    terminal.draw(|frame| {
-        let block = Block::bordered().title("Welcome");
-        let greeting = Paragraph::new("Hello, Ratatui! 🐭")
-            .centered()
-            .on_cyan()
-            .block(block);
-        frame.render_widget(greeting, frame.area());
-    })?;
-    std::thread::sleep(std::time::Duration::from_secs(5));
-    ratatui::restore();
-    Ok(())
+    ratatui::run(|terminal| {
+        terminal.draw(|frame| {
+            let block = Block::bordered().title("Welcome");
+            let greeting = Paragraph::new("Hello, Ratatui! 🐭")
+                .centered()
+                .yellow()
+                .block(block);
+            frame.render_widget(greeting, frame.area());
+        })?;
+        std::thread::sleep(std::time::Duration::from_secs(5));
+        Ok(())
+    })
 }
