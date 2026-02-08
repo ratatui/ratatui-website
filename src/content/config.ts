0cdf2e5cc3732b1015628a2c1f198ae554ae843b
JianDe <jiande2020@protonmail.com>
2026-02-09 01:33:56 +0800

    fix sidebar ordering for highlights #903

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,13 @@
+
 import { docsSchema } from "@astrojs/starlight/schema";
 import { defineCollection } from "astro:content";
+import { autoSidebarLoader } from "starlight-auto-sidebar/loader";
+import { autoSidebarSchema } from "starlight-auto-sidebar/schema";
 
 export const collections = {
   docs: defineCollection({ schema: docsSchema() }),
+  autoSidebar: defineCollection({
+    loader: autoSidebarLoader(),
+    schema: autoSidebarSchema(),
+  }),
 };
