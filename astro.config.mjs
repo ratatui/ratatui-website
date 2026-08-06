55f7c1f1ae0c2e3647b231bd0de3fa1f4a5478a1
BittuBarnwal7479 <bk8379903@gmail.com>
2026-08-06 17:42:13 +0530

    feat(docs): add 'Copy page as Markdown' action

--- astro.config.mjs
+++ astro.config.mjs
@@ -58,6 +58,7 @@ export default defineConfig({
       components: {
         Head: "./src/components/Head.astro",
         Header: "./src/components/Header.astro",
+        PageTitle: "./src/components/PageTitle.astro",
         ThemeSelect: "./src/components/ThemeSwitch.astro",
       },
       plugins: [
